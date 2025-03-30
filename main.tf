426a8615e73f79ee64b524b295f414bd772a802a
mal1610 <malcolmlau.1610@gmail.com>
2025-03-30 15:35:03 +0800

    fixed tflint issues

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.2.0"
+
   required_providers {
     aws = {
-    source  = "hashicorp/aws"
-    version = "5.93.0"
-   }
- }
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
-
+  
 provider "aws" {
   region = "ap-southeast-1"
 }
